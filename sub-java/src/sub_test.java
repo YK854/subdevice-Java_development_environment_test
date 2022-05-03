df8e3a7dcc3a891be7669ff75e0e2163794b963b
YK854 <g24hamo@gmail.com>
2022-05-03 23:57:10 +0900

    サブ機環境構築テスト

---
+++ sub-java/src/sub_test.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author skycr
+ *
+ */
+public class sub_test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("繰り返し（for）");
+		System.out.println();
+		for(int i=0; i<=5; i++) {
+			System.out.println("現在の回数は" + i + "回です！");
+			System.out.println("=========");
+		}
+		System.out.println("おわり");
+	}
+}
